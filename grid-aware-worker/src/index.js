c57e906f671d35ce7c0e093c24d9b9bacaba2040
Nic Chan <channicoleelise@gmail.com>
2025-08-15 14:12:57 +0800

    Add Grid aware worker

---
+++ grid-aware-worker/src/index.js
@@ -0,0 +1,44 @@
+import gridAwareAuto from '@greenweb/gaw-plugin-cloudflare-workers';
+
+const on = new HTMLRewriter()
+.on('body', {
+	element: (element) => {
+		element.setAttribute('data-gaw-mode', 'on');
+	},
+}).on('script', {
+	element: (element) => {
+		if (element.getAttribute('src')?.includes('GA-')) {
+			element.remove()
+		}
+	},
+})
+
+
+export default {
+	async fetch(request, env, ctx) {
+		return gridAwareAuto(request, env, ctx, {
+			gawDataApiKey: env.EMAPS_API_KEY,
+			debug: 'headers',
+			contentType: ['text/html', 'text/plain'],
+			devMode: false,
+			infoBar: {
+        target: "#gaw-info-bar",
+        learnMoreLink:
+          "https://www.thegreenwebfoundation.org/tools/grid-aware-websites/",
+        version: "latest",
+        popoverText:
+          "This website adapts based on your local electricity grid's carbon intensity",
+      },
+      htmlChanges: {
+				low: new HTMLRewriter()
+					.on('body', {
+						element: (element) => {
+							element.setAttribute('data-gaw-mode', 'off');
+						},
+					}),
+				moderate: on,
+				high: on,
+				}
+		});
+	},
+};
